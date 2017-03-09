8a7cc754a1de7f1a7d178af1d4e64c0979a2b49e
msmith <msmith@pki-cbg.de>
2017-03-09 15:55:11 +0000

    Bumped version in plugin file. Started working on user pref's.

--- src/main/java/DeformingMesh3DApp.java
+++ src/main/java/DeformingMesh3DApp.java
@@ -7,8 +7,14 @@ import ij.ImageJ;
 import ij.ImagePlus;
 import jogamp.nativewindow.jawt.JAWTUtil;
 
+import javax.swing.JFrame;
 import java.awt.EventQueue;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 
 /**
  *
@@ -17,7 +23,6 @@ import java.io.File;
  * User: msmith
  * Date: 7/2/13
  * Time: 8:01 AM
- * To change this template use File | Settings | File Templates.
  */
 public class DeformingMesh3DApp{
     static File input;
@@ -28,6 +33,11 @@ public class DeformingMesh3DApp{
         mf3d.addLights();
         SegmentationModel model = new SegmentationModel();
         SegmentationController control = new SegmentationController(model);
+        try{
+            loadProperties(control);
+        } catch(Exception e){
+            System.err.println("cannot load properties: " + e.getMessage());
+        }
         ControlFrame controller = new ControlFrame(control);
         controller.showFrame();
         RingController ring_control = new RingController(control);
@@ -35,10 +45,44 @@ public class DeformingMesh3DApp{
         controller.addTabbedPanel(ring_control.getContentPane(controller.getFrame()), "furrow");
         control.setMeshFrame3D(mf3d);
         model.setRingController(ring_control);
-
-
+        positionFrames(controller, mf3d);
         return control;
     }
+
+    static public void positionFrames(ControlFrame control, MeshFrame3D mf3d){
+        JFrame c = control.getFrame();
+        JFrame m = mf3d.getJFrame();
+        int x = m.getX();
+        int y = m.getY();
+        int w = m.getWidth();
+        int h = m.getHeight();
+        c.setLocation(x+w, y);
+
+    }
+
+    static public void loadProperties(SegmentationController control) throws IOException {
+        String home = System.getProperty("user.home");
+        File props = new File(home, ".dmesh3d");
+        if(props.exists()){
+            //try to read them properties.
+            List<String> lines = Files.readAllLines(props.toPath(), StandardCharsets.UTF_8);
+            for(String line: lines){
+                System.out.println(lines);
+            }
+        }
+    }
+
+    static public void saveProperties(SegmentationController control){
+        String home = System.getProperty("user.home");
+        File props = new File(home, ".dmesh3d");
+        if(props.exists()){
+            //try to read them properties.
+            props.delete();
+        }
+        BufferedWriter writer = Files.newBufferedWriter()
+
+    }
+
     private static void start3DApplication(){
         ImageJ.main(new String[]{});
 
