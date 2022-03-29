41ac4c6bc004cc65fb1d30e0870564d76628ab6d
Matthew Smith <mbs@here.now>
2022-03-29 14:47:01 +0200

    Adding key controls to the vedo mesh example. Not nescessarily a good idea.

--- src/python/examples/vedo-meshes.py
+++ src/python/examples/vedo-meshes.py
@@ -28,19 +28,24 @@ def showMeshes(plotter,  bmf_meshes , colors=None):
 		colors = [i for i in range(len(bmf_meshes))]
 	for i, bmesh in enumerate(bmf_meshes):
 		vmesh = getVedoMesh( bmesh )
-		vmesh.color(colors[i]).lineColor('black')
+		vmesh.color(colors[i]).alpha(0.5).lineColor("black")
 		x.append(vmesh)
 	plotter += x
 	return x
 
 used_colors = []
+
 def getColor( track_name ):
 	if track_name in used_colors:
 		return used_colors.index(track_name) + 1
 	used_colors.append(track_name)
 	return len(used_colors)
 
+			
+			
 if __name__ == "__main__":
+	save = False
+	
 	tracks = bmf.loadMeshTracks(sys.argv[1])
 	print("loaded %s tracks"%len(tracks) )
 	
@@ -50,22 +55,71 @@ if __name__ == "__main__":
 		for k in track.meshes:
 			frames.add(k)
 	
-	plotter = vedo.Plotter(interactive=False)
-	plotter.show()
-	save = False
-	while True:
-		for frame in frames:
+	frames = list(frames)
+	nFrames = len(frames)
+	
+	plotter = vedo.Plotter(interactive= not save)
+	plotter.index = 0;
+	plotter.meshes = []
+	plotter.running = True
+	
+	#def addCallback(self, eventName, func, priority=0.0) 
+	def finishPlotter( evt ):
+		print( "plotter clossing")
+		plotter.running = False
+		plotter.index = nFrames
+		plotter.closeWindow()
+		
+	def changeFrame(evt):
+		kp = evt['keyPressed']
+		if kp == 'comma':
+			plotter.index -= 1
+			if plotter.index < 0:
+				plotter.index  = nFrames - 1
+			
+			
+		elif kp == 'period':
+			plotter.index += 1
+			if plotter.index == nFrames:
+				plotter.index = 0
+		else:
+			if kp == 'b':
+				finishPlotter(evt)
+			return
+		plotter.remove(plotter.meshes)
+		plotter.meshes.clear()
+		frame = frames[plotter.index]
+		print("frame: %s index: %s"%(frame, plotter.index) )
+		to_see = []
+		colors = []
+		for track in tracks: 
+			if frame in track.meshes.keys():
+				to_see.append( track.meshes[frame] )
+				colors.append( getColor(track.name) )
+		plotter.meshes += showMeshes(plotter, to_see, colors)
+		plotter.render()
+	
+		
+	if not save:
+		plotter.addCallback("KeyPress", changeFrame)
+	while plotter.running:
+		while plotter.index < nFrames:
+			frame = frames[plotter.index]
+			print("frame: %s index: %s %s"%(frame, plotter.index, plotter) )
 			to_see = []
 			colors = []
 			for track in tracks: 
 				if frame in track.meshes.keys():
 					to_see.append( track.meshes[frame] )
 					colors.append( getColor(track.name) )
-			added = showMeshes(plotter, to_see, colors)
+			plotter.meshes += showMeshes(plotter, to_see, colors)
 			plotter.show()
+			
 			if save:
 				plotter.screenshot("snapshot-%04d.png"%frame)
-			
-			for item in added:
-				plotter.remove(item)
-		break;
+				plotter.index+=1
+		
+		
+			plotter.remove(plotter.meshes)
+			plotter.meshes.clear()
+		break
